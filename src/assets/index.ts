311a16da2ac80fad3e767987876f04ffb3b043d2
chocoboy08 <chocoboy0508@gmail.com>
2025-03-24 18:04:46 +0900

    chore: 아이콘 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as Email} from './icon_email.svg';
+export {default as Foreign} from './icon_foreign.svg';
+export {default as Lock} from './icon_lock.svg';
+export {default as Native} from './icon_native.svg';
+export {default as Person} from './icon_person.svg';
