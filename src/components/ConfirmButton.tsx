81f032dd0c36e892c2bcf89be2eaa9e7f592b601
chocoboy08 <chocoboy0508@gmail.com>
2025-03-26 15:46:24 +0900

    refactor: ConfirmButton에 style props 추가

--- src/components/ConfirmButton.tsx
+++ src/components/ConfirmButton.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import {GestureResponderEvent, Pressable, Text} from 'react-native';
+import {
+  GestureResponderEvent,
+  Pressable,
+  StyleProp,
+  Text,
+  ViewStyle,
+} from 'react-native';
 
 interface ConfirmButtonProps {
   title: string;
   handlerFn: (event: GestureResponderEvent) => void;
   active: boolean;
+  style?: StyleProp<ViewStyle>;
 }
 
-const ConfirmButton = ({title, handlerFn, active}: ConfirmButtonProps) => {
+const ConfirmButton = ({
+  title,
+  handlerFn,
+  active,
+  style,
+}: ConfirmButtonProps) => {
   return (
     <Pressable
       onPress={handlerFn}
-      style={{
-        backgroundColor: active ? '#1833DB' : '#D0D0D2',
-        width: 300,
-        height: 48,
-        borderRadius: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+      style={[
+        {
+          backgroundColor: active ? '#1833DB' : '#D0D0D2',
+          height: 48,
+          borderRadius: 40,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        style,
+      ]}
       disabled={!active}>
       <Text style={{fontSize: 16, fontWeight: 'bold', color: '#fff'}}>
         {title}
