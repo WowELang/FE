273ebb1b643918a067a5b5ad153f7ebb1919be40
chocoboy08 <chocoboy0508@gmail.com>
2025-05-01 13:06:42 +0900

    feat: 컬러시스템 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  white: '#ffffff',
+  blue: {
+    primary: '#1833DB',
+  },
+  black: {
+    primary: '#213815',
+  },
+  gray: {
+    primary: '#989A9F',
+    secondary: '#D0D0D2',
+  },
+  character: {
+    pink: '#E656FF',
+    green: '#2D9B4E',
+    yellow: '#FFBF00',
+    blue: '#51B4FF',
+    brown: '#A8674E',
+    red: '#FF8080',
+  },
+};
