83a5cd8aa31ca4b98febd7a14989bb473c9fb2bd
chocoboy08 <chocoboy0508@gmail.com>
2025-05-01 13:06:13 +0900

    feat: 바텀탭 네비게이션 세팅

---
+++ src/app/Board/screens/BoardScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const BoardScreen = () => {
+  return (
+    <View>
+      <Text>BoardScreen</Text>
+    </View>
+  );
+};
+
+export default BoardScreen;
