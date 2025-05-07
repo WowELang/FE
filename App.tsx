5afd5e52c1ce0a2b67caa48bb1d1030b8c4ef2c6
chocoboy08 <chocoboy0508@gmail.com>
2025-05-07 15:30:54 +0900

    feat: KeyboardAvoidnigView, 화면 밖 클릭 시 키보드 dismiss 기능 추가

--- App.tsx
+++ App.tsx
@@ -7,19 +7,41 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import LanguageScreen from './src/app/Login/screens/LanguageScreen';
 import LoginNavigator from './src/navigators/LoginNavigator';
+import RootNavigator from './src/navigators/RootNavigator';
 
 function App(): React.JSX.Element {
   const languageChosen = true;
+  const isLogined = false;
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer>
-          {languageChosen ? <LoginNavigator /> : <LanguageScreen />}
-        </NavigationContainer>
-      </SafeAreaView>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback style={{flex: 1}} onPress={Keyboard.dismiss}>
+          <SafeAreaView style={{flex: 1}}>
+            <NavigationContainer>
+              {languageChosen ? (
+                isLogined ? (
+                  <RootNavigator />
+                ) : (
+                  <LoginNavigator />
+                )
+              ) : (
+                <LanguageScreen />
+              )}
+            </NavigationContainer>
+          </SafeAreaView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaProvider>
   );
 }
