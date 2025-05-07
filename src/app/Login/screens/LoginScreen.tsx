8000e620e86799afd482ca4650c3a8a8f5ebae7c
chocoboy08 <chocoboy0508@gmail.com>
2025-05-07 15:37:24 +0900

    refactor: 로그인 화면 Typography, color 시스템 적용

--- src/app/Login/screens/LoginScreen.tsx
+++ src/app/Login/screens/LoginScreen.tsx
@@ -1,8 +1,10 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import ConfirmButton from '../../../components/ConfirmButton';
+import Typography from '../../../components/Typography';
 import UnderlinedInput from '../../../components/UnderlinedInput';
+import {colors} from '../../../constants/colors';
 import {LoginStackParamList} from '../../../navigators/LoginNavigator';
 
 type LoginScreenProps = {
@@ -17,15 +19,15 @@ const LoginScreen = ({navigation}: LoginScreenProps) => {
         flex: 1,
         paddingHorizontal: 38,
         backgroundColor: '#fff',
-        justifyContent: 'center',
-        paddingTop: 115,
-        paddingBottom: 300,
+        paddingTop: 120,
       }}>
       <View style={{alignItems: 'center', gap: 3}}>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Welcome to</Text>
-        <Text style={{fontSize: 28, fontWeight: 'bold', color: '#1833DB'}}>
+        <Typography size={20} bold>
+          Welcome to
+        </Typography>
+        <Typography size={28} color={colors.blue.primary} bold>
           와이랭
-        </Text>
+        </Typography>
       </View>
       <View style={{marginTop: 30}}>
         <UnderlinedInput
@@ -33,7 +35,6 @@ const LoginScreen = ({navigation}: LoginScreenProps) => {
           onChangeFn={text => setLoginData({...loginData, id: text})}
           placeholder="아이디"
           type="id"
-          errorMsg="잘못된 아이디입니다."
         />
         <UnderlinedInput
           value={loginData.password}
@@ -44,7 +45,13 @@ const LoginScreen = ({navigation}: LoginScreenProps) => {
       </View>
       <View style={{marginTop: 17}}>
         <View style={{gap: 8}}>
-          <ConfirmButton title="로그인" handlerFn={() => {}} active />
+          <ConfirmButton
+            title="로그인"
+            handlerFn={() => {
+              navigation.navigate('Interest');
+            }}
+            active
+          />
           <ConfirmButton
             title="이메일 회원가입"
             handlerFn={() => {
@@ -59,15 +66,12 @@ const LoginScreen = ({navigation}: LoginScreenProps) => {
             alignItems: 'center',
             marginTop: 30,
           }}>
-          <Text
-            style={{
-              fontSize: 12,
-              color: '#989A9F',
-              borderBottomWidth: 0.75,
-              borderBottomColor: '#989A9F',
-            }}>
+          <Typography
+            size={12}
+            color={colors.gray.primary}
+            style={{borderBottomWidth: 0.75, borderColor: colors.gray.primary}}>
             {'아이디 / 비밀번호 찾기 >'}
-          </Text>
+          </Typography>
         </View>
       </View>
     </View>
