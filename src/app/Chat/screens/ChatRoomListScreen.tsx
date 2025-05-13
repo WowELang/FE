af1151a9c237a6edfd10f7ea1bd0d15994dcdd6d
chocoboy08 <chocoboy0508@gmail.com>
2025-05-13 21:38:51 +0900

    feat: 채팅방 목록 스크린 추가

---
+++ src/app/Chat/screens/ChatRoomListScreen.tsx
@@ -0,0 +1,45 @@
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React from 'react';
+import {View} from 'react-native';
+import Divider from '../../../components/Divider';
+import {colors} from '../../../constants/colors';
+import {ChatstackParamList} from '../../../navigators/ChatNavigator';
+import ChatRoom from '../components/ChatRoom';
+
+const ChatRoomListScreen = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<ChatstackParamList, 'ChatRoomList'>>();
+  return (
+    <View>
+      <ChatRoom
+        roomData={{
+          id: 'af',
+          participants: ['민수', '준수'],
+          createdAt: '',
+          updatedAt: '2025-05-09T10:10:53.099Z',
+          lastMessage: {
+            id: '1',
+            roomId: 'string',
+            senderId: 'string',
+            type: 'TEXT',
+            content: '안녕 홍냥아!!!!!',
+            s3Key: 'string',
+            originalMessageText: 'string',
+            correctedText: 'string',
+            createdAt: 'string',
+            deleted: true,
+          },
+          unreadCount: 0,
+        }}
+        onPressFn={() => {
+          navigation.navigate('Chat', {roomId: 'af'});
+          console.log('hi');
+        }}
+      />
+      <Divider color={colors.gray.secondary} />
+    </View>
+  );
+};
+
+export default ChatRoomListScreen;
